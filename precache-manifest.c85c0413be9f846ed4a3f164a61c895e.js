4f78633bf90fb04ec39f02af016f6fb0e4c217d3
d00d1e <lee.julianay@gmail.com>
2020-09-26 11:01:51 -0700

    Updates

---
+++ precache-manifest.c85c0413be9f846ed4a3f164a61c895e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc10686a16066ff18018fd4c62a794ab",
+    "url": "/react-flashcards/index.html"
+  },
+  {
+    "revision": "5abaa7077459bb41a0d0",
+    "url": "/react-flashcards/static/css/main.19b8c04a.chunk.css"
+  },
+  {
+    "revision": "e5491c28358dabacc1d4",
+    "url": "/react-flashcards/static/js/2.140d5fb9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-flashcards/static/js/2.140d5fb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5abaa7077459bb41a0d0",
+    "url": "/react-flashcards/static/js/main.d832ad2a.chunk.js"
+  },
+  {
+    "revision": "a13e408e76d992f58d34",
+    "url": "/react-flashcards/static/js/runtime-main.c9f27840.js"
+  }
+]);
